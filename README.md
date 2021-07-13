aee658cbba28b6023b1ea96a94f0ca61dc76f661
nihelili14 <ybj_at_14@163.com>
2021-07-13 15:12:58 +0800

    modify in 7/13

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LearnShell
 入门学习Shell
+这个仓库用于记录学习的笔记
---
+++ shell变量/variable1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+a=4
+name="nihelili"
+echo $a
+echo $name
+
+readonly name
+#name="14"               #报错
+
+#unset name
+#echo $name
